aa35653d8c87a30cb16b7df70909aa058223e896
Nathan Rajlich <nathan@tootallnate.net>
2015-04-30 20:02:54 -0700

    gc: remove console.log() calls, check `err` arg

--- gc.js
+++ gc.js
@@ -4,24 +4,22 @@ var redis = require('redis').createClient(config.redis);
 
 sub.on('psubscribe', function (pattern, count) {
   console.log('"psubscribe" event. pattern=%j count=%j', pattern, count);
-  console.log(arguments);
 });
 
 sub.on('pmessage', function (pattern, channel, message) {
   console.log('"pmessage" event. pattern=%j channel=%j message=%j', pattern, channel, message);
-  console.log(arguments);
 
   if ('expired' === message || 'del' === message) {
     // get post that the session was visiting
     var socketId = channel.split(':').pop();
 
     redis.hget('viewers', socketId, function (err, postId) {
-      console.log(arguments);
+      if (err) throw err;
       redis.multi()
         .srem('viewers:' + postId, socketId)
         .hdel('viewers', socketId)
         .exec(function (err, results) {
-          console.log(arguments);
+          if (err) throw err;
         });
     });
   }
